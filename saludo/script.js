78cbfdfbc4149aa26963109846f2bf862839f572
juliofloresp <julioflorespadron@gmail.com>
2022-06-17 15:20:05 -0500

    Haciendo html css y jss

--- saludo/script.js
+++ saludo/script.js
@@ -1,6 +1,20 @@
-console.log ("Se nota que me extrañas");
+console.log ("Se nota que todavía me extrañas");
 
-h1 {
-    background-color: black;
-    color: beige
+setInterval(cambiarSaludo, 2000);
+
+function cambiarSaludo(){
+    let frase = document.getElementsByTagName("h2")[1];
+    if(frase.innerHTML == "Si como siempre"){
+        frase.innerHTML = "Impongo moda para las niñas!";
+    }
+    else{
+        frase.innerHTML = "Gracias como siempre";
+    }
+   // console.log ("FRASE :" + frase);
 }
+
+
+
+
+
+
